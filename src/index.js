df0aca1ced580634a92a97a12fe2bcf605f390d4
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-03-06 16:32:10 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,22 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    
+    let result = [];
+    let letter = '', final_letter = '';
+    let final_res = '';
+
+    for( let i = 0; i < expr.length; i+=10){
+        letter = expr.slice(i, i+10);
+        final_letter = letter.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-');
+
+        if(letter == "**********") result.push(" ");
+        result.push(MORSE_TABLE[final_letter]);
+        
+      }
+      final_res = result.join("");
+      
+    return final_res;
 }
 
 module.exports = {
